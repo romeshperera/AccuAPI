8e74e406073b71fe65f365e330e67d63ff374c6e
vp.romesh.perera <romeshperera@virtusa.com>
2018-06-19 17:58:06 -0400

    -

---
+++ Services/AccuNetAPI/Controllers/NetController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AccuNetAPI.Controllers
+{
+    public class NetController : ApiController
+    {
+        // GET: api/Net
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Net/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Net
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Net/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Net/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
